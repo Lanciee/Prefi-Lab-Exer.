2a657a449ad5dd960b7af6b0dd4c7abc2f302f36
Lance Angelo Pagdato <116186082+Lanciee@users.noreply.github.com>
2022-11-24 07:46:07 +0800

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,11 @@
+public class Dog extends Animal{
+	public void eat(){
+		System.out.println("Dogs love to eat seeds");
+	}
+	public void sleep(){
+		System.out.println("Sleep for 12-10 hours a day");
+	}
+	public void makeSound(){
+		System.out.println("Arf Arf");
+	}
+}
